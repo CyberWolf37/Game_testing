40be02c9e12920bde969e220ec534a2cab658b9f
CyberWolf <borel.loic.37@gmail.com>
2020-10-17 13:36:07 +0200

    add some code

--- src/states/menu/menu.rs
+++ src/states/menu/menu.rs
@@ -6,7 +6,16 @@ use amethyst::{
     renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
 };
 
+const BUTTON_START: &str = "start";
+const BUTTON_LOAD: &str = "load";
+const BUTTON_OPTIONS: &str = "options";
+const BUTTON_CREDITS: &str = "credits";
+
 pub struct Menu{
+    ui_root: Option<Entity>,
+    button_start: Option<Entity>,
+    button_options: Option<Entity>,
+    button_credits: Option<Entity>,
 }
 
 impl SimpleState for Menu {
@@ -15,15 +24,25 @@ impl SimpleState for Menu {
 
         let world = data.world;
 
-        world.register::<Items>();
-        
+        self.ui_root =
+        Some(world.exec(|mut creator: UiCreator<'_>| creator.create("config/ui/menu.ron", ())));
     }
-}
 
-fn initialise_camera(world: &mut World){
-    let mut transform = Transform::default();
-    transform.set_translation_xyz(ARENA_WIDTH * 0.5, ARENA_HEIGHT * 0.5, 1.0);
+    fn update(&mut self, state_data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        // only search for buttons if they have not been found yet
+        let StateData { world, .. } = state_data;
 
-    world
-        .create_enti
+        if self.button_start.is_none()
+            || self.button_options.is_none()
+            || self.button_credits.is_none()
+        {
+            world.exec(|ui_finder: UiFinder<'_>| {
+                self.button_start = ui_finder.find(BUTTON_START);
+                self.button_options = ui_finder.find(BUTTON_OPTIONS);
+                self.button_credits = ui_finder.find(BUTTON_CREDITS);
+            });
+        }
+
+        Trans::None
+    }
 }
